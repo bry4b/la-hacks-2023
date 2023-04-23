97cd6413415870d6a20db6f223f8d15d3cc8ab4c
bry4b <116151219+bry4b@users.noreply.github.com>
2023-04-22 23:35:34 -0700

    fix keyhook

--- desktopapp/RunWindow.cs
+++ desktopapp/RunWindow.cs
@@ -12,42 +12,34 @@ using System.Windows.Forms;
 using Python.Runtime;
 using System.Windows;
 using System.Windows.Input;
+using NonInvasiveKeyboardHookLibrary;
 
 
 namespace desktopapp
 {
     public partial class RunWindow : Form
     {
-        private LowLevelKeyboardListener _listener;
 
         public RunWindow()
         {
+            var kbhook = new KeyboardHookManager();
+            kbhook.Start();
+
             InitializeComponent();
-            PythonEngine.Initialize();
-            this.TopMost = true;
-        }
-        private void Window_Loaded(object sender, EventArgs e)
-        {
-            _listener = new LowLevelKeyboardListener();
-            _listener.OnKeyPressed += _listener_OnKeyPressed;
+/*            PythonEngine.Initialize();
+*/            this.TopMost = true;
 
-            _listener.HookKeyboard();
-        }
+            kbhook.RegisterHotkey(0x18, () =>
+            {
+                this.Close();
+            });
 
-        void _listener_OnKeyPressed(object sender, KeyPressedArgs e)
-        {
         }
-
-        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
-        {
-            _listener.UnHookKeyboard();
-        }
-
         private void Form1_Load(object sender, EventArgs e)
         {
         }
 
-        private bool isRecording = false;
+        /*private bool isRecording = false;
         private void Record()
         {
             MessageBox.Show("yes");
@@ -63,7 +55,7 @@ namespace desktopapp
                 {
                     MessageBox.Show("error: " + ex.Message);
                 }
-
+*/
 /*                dynamic module = Py.Import("..\\..\\..\\src\\microphone.py");
                 dynamic recorder = module.Recorder();*/
 /*                if (!isRecording)
@@ -80,82 +72,7 @@ namespace desktopapp
                     MessageBox.Show("stopped recording");
 
                 }*/
-            }
-        }
+     /*       }
+        }*/
     }
-
-    #region keyboard hook
-    public class LowLevelKeyboardListener
-    {
-        private const int WH_KEYBOARD_LL = 13;
-        private const int WM_KEYDOWN = 0x0100;
-        private const int WM_SYSKEYDOWN = 0x0104;
-
-        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-        private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
-
-        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-        [return: MarshalAs(UnmanagedType.Bool)]
-        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
-
-        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
-
-        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-        private static extern IntPtr GetModuleHandle(string lpModuleName);
-
-        public delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
-
-        public event EventHandler<KeyPressedArgs> OnKeyPressed;
-
-        private LowLevelKeyboardProc _proc;
-        private IntPtr _hookID = IntPtr.Zero;
-
-        public LowLevelKeyboardListener()
-        {
-            _proc = HookCallback;
-        }
-
-        public void HookKeyboard()
-        {
-            _hookID = SetHook(_proc);
-        }
-
-        public void UnHookKeyboard()
-        {
-            UnhookWindowsHookEx(_hookID);
-        }
-
-        private IntPtr SetHook(LowLevelKeyboardProc proc)
-        {
-            using (Process curProcess = Process.GetCurrentProcess())
-            using (ProcessModule curModule = curProcess.MainModule)
-            {
-                return SetWindowsHookEx(WH_KEYBOARD_LL, proc, GetModuleHandle(curModule.ModuleName), 0);
-            }
-        }
-
-        private IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
-        {
-            if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN || wParam == (IntPtr)WM_SYSKEYDOWN)
-            {
-                int vkCode = Marshal.ReadInt32(lParam);
-
-                if (OnKeyPressed != null) { OnKeyPressed(this, new KeyPressedArgs(KeyInterop.KeyFromVirtualKey(vkCode))); }
-            }
-
-            return CallNextHookEx(_hookID, nCode, wParam, lParam);
-        }
-    }
-
-    public class KeyPressedArgs : EventArgs
-    {
-        public Key KeyPressed { get; private set; }
-
-        public KeyPressedArgs(Key key)
-        {
-            KeyPressed = key;
-        }
-    }
-    #endregion
 }
